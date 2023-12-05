04ece58ee7ee6d1e9cf7349c955a52782d69119a
William Felton <williamfelton2006@icloud.com>
2023-12-05 14:31:53 +0000

    the server can respond and send back coordinates

--- src/MainNEA.py
+++ src/MainNEA.py
@@ -178,13 +178,16 @@ class Game:
 
             circle = pygame.draw.circle(self.window, (255, 0, 0), (int(self.player.coords.x), int(self.player.coords.y)),
                                         self.player.radius)
-            coordinatesx= str(str(self.player.coords.x) + " " + str(self.player.coords.y))
-            coordinatesy = str(self.player.coords.y)
-            packetsToSend = str.encode(coordinatesx)
+            coordinates= str(str(self.player.coords.x) + " " + str(self.player.coords.y))
+            packetsToSend = str.encode(coordinates)
             serverAddressPort = ("127.0.0.1", 20001)
             buffersize = 1024
             UDPclientsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
             UDPclientsocket.sendto(packetsToSend, serverAddressPort)
+            message = UDPclientsocket.recvfrom(buffersize)
+            coordinates = str(message[0]).strip("b")
+            finalcoords = format(coordinates)
+            print(finalcoords)
             pygame.Rect.clamp(circle, self.player.circle_hbox)
 
             self.clock.tick(self.FPS)
--- src/Server.py
+++ src/Server.py
@@ -5,12 +5,9 @@ localPort = 20001
 bufferSize = 1024
 msgFromServer = "Client Connected"
 sentmessage = str.encode(msgFromServer)
-# Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
 print("UDP server up and listening")
-# Listen for incoming datagrams
 
 while(True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -19,5 +16,5 @@ while(True):
     clientMsg = "Message from Client:{}".format(message)
     clientIP  = "Client IP Address:{}".format(address)
     print(clientMsg)
-    # Sending a reply to client
-    UDPServerSocket.sendto(sentmessage, address)
+    playercoords = message
+    UDPServerSocket.sendto(playercoords, address)
